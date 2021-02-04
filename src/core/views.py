b7539c91940aca5f6147b8d99e20e1bb5cdf5f85
Dendy <dendyjuliano2019@gmail.com>
2021-02-04 14:58:59 +0700

    build get and post data

--- src/core/views.py
+++ src/core/views.py
@@ -5,17 +5,21 @@ from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .serializers import PostSerializer
+from .models import Post
+
 class TestView(APIView):
     def get(self,request,*args,**kwargs):
-        data = {
-            'name':'jhon',
-            'age':17,
-        }
-        return Response(data)
+        qs = Post.objects.all()
+        post = qs.first()
+        # serializer = PostSerializer(qs, many=True)  #for multiple response data
+        serializer = PostSerializer(post) #for one response data
+        return Response(serializer.data)
 
-# def test_view(request):
-    # data = {
-    #     'name':'jhon',
-    #     'age':17,
-    # }
-#     return JsonResponse(data)
+    def post(self,request,*args,**kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
--- src/django_api/urls.py
+++ src/django_api/urls.py
@@ -4,4 +4,6 @@ from core.views import TestView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_frame
+    path('api-auth/', include('rest_framework.urls')),
+    path('',TestView.as_view(),name='test'),
+]
