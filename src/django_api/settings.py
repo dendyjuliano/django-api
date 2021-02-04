9a011ad10058e531b25d4a28f1104cfe14065eff
Dendy <dendyjuliano2019@gmail.com>
2021-02-04 19:16:03 +0700

    django-rest-auth

--- src/django_api/settings.py
+++ src/django_api/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
--- src/django_api/urls.py
+++ src/django_api/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework.authtoken.views import obtain_auth_token
+
 from core.views import TestView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('rest-auth/', include('rest_auth.urls')),
     path('',TestView.as_view(),name='test'),
+    path('api/token/',obtain_auth_token,name='obtain-token'),
 ]
